bed946e0c1dc4ed23417a22c1a257736f14f2c56
vmgn16 <mg_619@hotmail.com>
2025-04-22 00:12:50 -0600

    mod db.php

--- includes/db.php
+++ includes/db.php
@@ -3,10 +3,13 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/Config.php';
 
 use App\Config;
-//use Dotenv\Dotenv;
+use Dotenv\Dotenv;
 
-//$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-//$dotenv->load();
+// Carga el archivo .env si existe
+if (file_exists(__DIR__ . '/../.env')) {
+    $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+}
 
 try {
     $host = Config::get('DB_HOST');
