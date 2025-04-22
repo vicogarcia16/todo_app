30f18f15b14522f7409f4e59e9ed72c9502bafbd
vmgn16 <mg_619@hotmail.com>
2025-04-22 00:10:58 -0600

    mod db.php

--- includes/db.php
+++ includes/db.php
@@ -3,10 +3,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/Config.php';
 
 use App\Config;
-use Dotenv\Dotenv;
+//use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+//$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+//$dotenv->load();
 
 try {
     $host = Config::get('DB_HOST');
